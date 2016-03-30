300b705e95f660f3005c8ba675fa86ec66d84196
veggiedefender <jessejesse123@gmail.com>
2016-03-30 15:43:31 -0400

    spiced up the layout a bit :^)

--- calculus-generator/graph.py
+++ calculus-generator/graph.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt, numpy as np
+import matplotlib.axes as axes
 import StringIO
 from math import *
 
+#import webbrowser
+
 def graph(function, range = 10):
-    plt.plot([ eval(function) for x in np.arange(0,5,0.001) ])
     sio = StringIO.StringIO()
-    plt.savefig(sio, format="png")
+
+    plt.plot([ eval(function) for x in np.arange(-5, 5, 1) ])
+    plt.axhline(0, color="black")
+    plt.axvline(0, color="black")
+
+    plt.savefig(sio, format="png", transparent=True)
     img_b64 = sio.getvalue().encode("base64").strip()
+
     plt.clf()
     sio.close()
     return "data:image/png;base64,%s" % img_b64
+
+#with open("THING.html", "w+") as f:
+#    f.write("<img src='%s'/>" % graph("x**3"))
+#webbrowser.open("THING.html", new=0, autoraise=True)#
