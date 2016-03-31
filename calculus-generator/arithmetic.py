49fef34e6f56b80c22cdba9799a4db34a7734e8b
veggiedefender <jessejesse123@gmail.com>
2016-03-31 11:49:14 -0400

    arithmetic uses floats now cause division

--- calculus-generator/arithmetic.py
+++ calculus-generator/arithmetic.py
@@ -28,11 +28,11 @@ def arithmetic(pType, cap):
     a = random.randint(1, cap)
     b = random.randint(1, cap)
 
-    payload[0]["choices"] = [a + b, a - b, a * b, a / b]
+    payload[0]["choices"] = [a + b * 1.0, a - b * 1.0, a * b * 1.0, a / b * 1.0]
     random.shuffle(payload[0]["choices"])
 
     payload[0]["problem"] = "%s %s %s = " % (a, operator, b)
-    payload[0]["solution"] = operators[operator](a, b)
+    payload[0]["solution"] = operators[operator](a * 1.0, b)
     payload[0]["optional"] = False
 
     payload[0]["image"] = "http://eonothem.github.io/images/arithmetic.png"
