1743248f48df5f9c40ca89fabeefe07ee6f068a1
veggiedefender <jessejesse123@gmail.com>
2016-05-12 10:14:09 -0400

    Jimmy

--- calculus-generator/areaVolume.py
+++ calculus-generator/areaVolume.py
@@ -7,30 +7,65 @@ x = symbols('x')
 #Simple program for finding area/volume/etc bounded between two curves in the first quadrant
 #I'm only using the first quadrant & simple polynomials because this gets complicated real fast if you don't: and that takes effort
 def generateBoundingFunctions(lower, upper):
-	precision = 5
-	c = random.randint(1,10)
-	b = random.randint(1,10)
-	g = random.randint(0,5)
-	functionOne = c*x+g
-	functionTwo = b*x**2
-	intercepts = solve(functionOne-functionTwo,x)
-	boundVal = 0
-	if(intercepts[0] > 0):
-		boundVal = intercepts[0]
-	else:
-		boundVal = intercepts[1]
+    precision = 5
+    c = random.randint(1,10)
+    b = random.randint(1,10)
+    g = random.randint(0,5)
+    functionOne = c*x+g
+    functionTwo = b*x**2
+    intercepts = solve(functionOne-functionTwo,x)
+    boundVal = 0
+    if(intercepts[0] > 0):
+        boundVal = intercepts[0]
+    else:
+        boundVal = intercepts[1]
 
-	boundVal = N(boundVal,precision)
-	
-	area = integrate(functionOne-functionTwo, (x,0,boundVal))
+    boundVal = N(boundVal,precision)
+    
+    area = integrate(functionOne-functionTwo, (x,0,boundVal))
 
-	volumeX = N(pi,precision)*integrate(functionOne**2-functionTwo**2, (x,0,boundVal))
+    volumeX = N(pi,precision)*integrate(functionOne**2-functionTwo**2, (x,0,boundVal))
 
-	
-	
-	#print(intercepts[1] < 0)
-	
+    volumeSquareBase = integrate((functionOne-functionTwo)**2, (x,0,boundVal))
+
+    verticalAxis = random.randint(-4,0)
+    #print(verticalAxis)
+    #print(functionOne, functionTwo)
+    volumeShell = 2*N(pi,precision)*integrate((x-verticalAxis)*(functionOne-functionTwo), (x,0,boundVal))
+    #print(volumeShell)
+    payload = []
+
+    output = dict()
+    output["header"] = "The functions "+latex(functionOne)+" and "+latex(functionTwo)+" are defined in the first quadrant."
+    output["question"] = "Find the area between the two functions."
+    output["solution"] = area
+    payload.append(output)
+
+    output = dict()
+    output["question"] = "Find the volume of the solid of revolution of the two functions when rotated about the x-axis."
+    output["solution"] = volumeX
+    payload.append(output)
+
+    output = dict()
+    output["question"] = "Find the volume of the solid of revolution between the two functions when it is rotated about the line x = "+str(verticalAxis)
+    output["solution"] = volumeShell
+    payload.append(output)
+
+    output = dict()
+    output["question"] = "Find the volume between the two functions when the cross-section perpendicular to the x-axis is a square."
+    output["solution"] = volumeSquareBase
+    payload.append(output)
 
 
+    return payload
+  
+    
 
-generateBoundingFunctions(1,3)
+def latex(func):
+    func = str(func)
+    func = func.replace("**", "^")
+    func = func.replace("*", "")
+    return func
+
+import pprint
+pprint.pprint(generateBoundingFunctions(1,3))
---
+++ calculus-generator/physics.py
@@ -0,0 +1,56 @@
+from sympy import *
+import differentiation
+
+import random
+t = symbols('t')
+
+#Simple program for finding area/volume/etc bounded between two curves in the first quadrant
+#I'm only using the first quadrant & simple polynomials because this gets complicated real fast if you don't: and that takes effort
+def generate():
+    precision = 3
+    a = random.randint(1,10)
+    b = random.randint(1,10)
+    c = random.randint(1,10)
+    d = random.randint(1,10)
+
+    velocity = a*cos(b*t+c)+d
+
+    accelerationTime = random.randint(0,100)
+
+    payload = []
+    item = "dat boi (o shit waddup)"
+    output = dict()
+    output["header"] = "The velocity of %s over time is given by the function %s" % (item, latex(str(velocity)))
+    output["question"] = "Calculate the instantanious acceleration of the meme at time t = "+str(accelerationTime)
+    output["solution"] = N(diff(velocity,t).subs(t,accelerationTime), precision)
+    payload.append(output)
+
+    positionTime = random.randint(0,10)
+    position = integrate(velocity,t)+random.randint(0,50)
+    #print(position)
+
+    output = dict()
+    output["question"] = "At the time t = %s, the position of %s is equal to %s. Find the equation for postion." % (positionTime,item,N(position.subs(t,positionTime),precision))
+    output["solution"] = position
+    payload.append(output)
+
+    averageVelocityStart = random.randint(0,10)
+    averageVelocityEnd = averageVelocityStart+random.randint(4,20)
+
+    output = dict()
+    output["question"] = "Find the average velocity of %s from %s<t<%s" % (item, averageVelocityStart, averageVelocityEnd)
+    output["solution"] = N( (1.0/(averageVelocityEnd-averageVelocityStart))*integrate(velocity,(t,averageVelocityStart,averageVelocityEnd)), precision ) 
+
+    payload.append(output)
+
+    return payload
+    
+
+def latex(func):
+    func = str(func)
+    func = func.replace("**", "^")
+    func = func.replace("*", "")
+    return func
+
+import pprint
+pprint.pprint(generate())
